df378950ae82d2a5332247110127ceac0a05c531
axie10 <cryptosushi21@gmail.com>
2025-07-01 19:02:11 +0200

    Implement Task struct and external functions for task management

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -6,4 +6,30 @@ pragma solidity ^0.8.24;
 
 contract ToDoList {
 
+
+    struct Task {
+        string title;
+        string description;
+        bool completed;
+    }
+
+    // Variables
+    Task[] public tasks;
+
+    // Modifiers
+
+    // Events
+
+    // External functions
+    function createTask(string memory title_, string memory description_) public {
+        Task memory task1 = Task( title_, description_, false );
+        tasks.push(task1);
+    }
+
+    function showTasks() public view returns(Task[] memory){
+        return tasks;
+    }
+
+    // Internal functions
+
 }
